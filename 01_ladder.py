18efc2db27a4060fee0e777d98ad98ccfc4870aa
AdiletEngineering <adilet.joldoshbek@gmail.com>
2020-01-20 21:49:49 +0600

    used print(f)

--- 01_ladder.py
+++ 01_ladder.py
@@ -1,7 +1,10 @@
-print("please type words separated by space")
+print("please type words separated by space | limit 100 symbols")
 string1 = input(str())
-print("-------------------------------------------------------------------")
-
-array = string1.split(" ")
-for i in range(len(array)):
-	print("\t"*i + array[i])
+if len(string1) > 100:
+	print("строка превышает 100 символов")
+else:
+	print("-------------------------------------------------------------------")
+	array = string1.split(" ")
+	for i in range(len(array)):
+		print(f"{i+1}: {'   '*i}{array[i]}")
+		# print("\t"*i + array[i])                  ##another way
